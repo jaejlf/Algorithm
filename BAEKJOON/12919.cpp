4808332a1fd96810f915e6c6b975c2e06be1a504
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-12-26 17:41:41 +0900

    12919 : A와 B 2

--- BAEKJOON/12919.cpp
+++ BAEKJOON/12919.cpp
@@ -3,12 +3,12 @@
 #define TRUE 1
 #define FALSE 0
 using namespace std;
- 
+
 string popA(string s) {
     s.pop_back();
     return s;
 }
- 
+
 string rev(string s) {
     reverse(s.begin(), s.end());
     s.pop_back();
@@ -17,33 +17,36 @@ string rev(string s) {
 
 int compare(string s, string t) {
     if (s.size() == t.size()) {
-        if (s == t) return TRUE;
-        else return FALSE;
+        if (s == t)
+            return TRUE;
+        else
+            return FALSE;
     }
     if (t.size() >= 1) {
-        if (t.front() == 'A' && t.back() == 'A')
+        if (t.front() == 'A' && t.back() == 'A') {
             return compare(s, popA(t));
- 
-        if (t.front() == 'A' && t.back() == 'B')
+        }
+
+        if (t.front() == 'A' && t.back() == 'B') {
             return FALSE;
- 
+        }
+
         if (t.front() == 'B' && t.back() == 'A') {
             return compare(s, popA(t)) || compare(s, rev(t));
         }
- 
+
         if (t.front() == 'B' && t.back() == 'B') {
             return compare(s, rev(t));
         }
     }
 }
- 
+
 int main() {
     string s, t;
     cin >> s >> t;
-    if(compare(s,t)){
-      cout << TRUE;
+    if (compare(s, t)) {
+        cout << TRUE;
+    } else {
+        cout << FALSE;
     }
-    else{
-      cout << FALSE;
-    }  
 }
