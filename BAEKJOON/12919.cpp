25b7d9d32c0fa82add0efa522d3afca6f6df8e95
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-12-26 14:01:59 +0900

    12919 : A와 B 2

---
+++ BAEKJOON/12919.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define TRUE 1
+#define FALSE 0
+using namespace std;
+ 
+string popA(string s) {
+    s.pop_back();
+    return s;
+}
+ 
+string rev(string s) {
+    reverse(s.begin(), s.end());
+    s.pop_back();
+    return s;
+}
+
+int compare(string s, string t) {
+    if (s.size() == t.size()) {
+        if (s == t) return TRUE;
+        else return FALSE;
+    }
+    if (t.size() >= 1) {
+        if (t.front() == 'A' && t.back() == 'A')
+            return compare(s, popA(t));
+ 
+        if (t.front() == 'A' && t.back() == 'B')
+            return FALSE;
+ 
+        if (t.front() == 'B' && t.back() == 'A') {
+            return compare(s, popA(t)) || compare(s, rev(t));
+        }
+ 
+        if (t.front() == 'B' && t.back() == 'B') {
+            return compare(s, rev(t));
+        }
+    }
+}
+ 
+int main() {
+    string s, t;
+    cin >> s >> t;
+    if(compare(s,t)){
+      cout << TRUE;
+    }
+    else{
+      cout << FALSE;
+    }  
+}
