e01576a9c67a761c9bd585e5c0d27504dc1e7835
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-02-13 17:46:26 +0900

    1074 : Z

--- BAEKJOON/1074.cpp
+++ BAEKJOON/1074.cpp
@@ -1,6 +1,5 @@
-//Z
+// Z
 #include <math.h>
-
 #include <iostream>
 
 using namespace std;
@@ -8,19 +7,19 @@ using namespace std;
 int N, r, c;
 int result;
 
-void Z(int y, int x, int size) {
-    if (y == r && x == c) {
+void Z(int row, int col, int size) {
+    if (row == r && col == c) {
         cout << result << endl;
         return;
     }
 
-    if (r < y + size && r >= y && c < x + size && c >= x) {
-        Z(y, x, size / 2);                        // 1사분면
-        Z(y, x + size / 2, size / 2);             // 2사분면
-        Z(y + size / 2, x, size / 2);             // 3사분면
-        Z(y + size / 2, x + size / 2, size / 2);  // 4사분면
+    if (r < row + size && r >= row && c < col + size && c >= col) {  //현재 4분면 내에 존재
+        Z(row, col, size / 2);                                     // 1사분면
+        Z(row, col + size / 2, size / 2);                          // 2사분면
+        Z(row + size / 2, col, size / 2);                          // 3사분면
+        Z(row + size / 2, col + size / 2, size / 2);               // 4사분면
 
-    } else {
+    } else {  //현재 4분면 내에 존재하지 않음
         result += size * size;
     }
 }
