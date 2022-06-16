85ff32c4bbb6c49ae6ae667e6e623a42336f8bab
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-06-16 19:56:36 +0900

    Hash 1 : 완주하지 못한 선수 / hash

---
+++ Programmers/고득점 Kit/hash1_sol2.java
@@ -0,0 +1,28 @@
+// 완주하지 못한 선수
+// hash
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String par : participant) {
+            map.put(par, map.getOrDefault(par, 0) + 1);
+        }
+        for (String com : completion) {
+            map.put(com, map.get(com) - 1);
+        }
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+
+        return answer;
+
+    }
+}
