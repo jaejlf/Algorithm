ea2abf619d3c175cf41e5b010e30761f35fb1a89
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-06-16 18:26:49 +0900

    JAVA Basic String

---
+++ Basic_Practice/string.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String[] args) {
+
+        String str = "abcde";
+        System.out.println(str.charAt(3));
+
+        System.out.println(str.equals("aaaaa"));
+        System.out.println(str.equals("abcde"));
+
+    }
+}
