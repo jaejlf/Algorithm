ac544bb953672c93dd786ff320af2b07506f4fcb
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-06-16 19:28:31 +0900

    Hash 1 : 완주하지 못한 선수 / sort

---
+++ Programmers/고득점 Kit/hash1_sol1.java
@@ -0,0 +1,22 @@
+
+// 완주하지 못한 선수
+// sort
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if (!completion[i].equals(participant[i])) {
+                return participant[i];
+            }
+        }
+
+        return participant[participant.length - 1];
+
+    }
+}
