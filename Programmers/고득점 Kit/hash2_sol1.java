29c292cb92a13f07050e78888d649fbae0377b8c
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-06-16 21:59:05 +0900

    Hash 2 : 전화번호 목록 / sort, startsWith

---
+++ Programmers/고득점 Kit/hash2_sol1.java
@@ -0,0 +1,17 @@
+// 전화번호 목록 - sort, startsWith
+
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
+        }
+
+        return true;
+
+    }
+}
