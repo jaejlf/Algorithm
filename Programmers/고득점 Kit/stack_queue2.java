807dedac306cc107c24a416d2281dcca7c70356b
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-06-23 17:05:17 +0900

    Stack-Queue 2 : 프린터

---
+++ Programmers/고득점 Kit/stack_queue2.java
@@ -0,0 +1,31 @@
+// 프린터
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int priority : priorities) {
+            pq.add(priority);
+        }
+
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+
+    }
+}
